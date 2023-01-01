ec59bd20f3a2503fb1877a59f4a78edee4893e3f
AbdallahZein12 <94486924+AbdallahZein12@users.noreply.github.com>
2023-01-01 18:28:46 -0500

    Update main.py

--- day_1/main.py
+++ day_1/main.py
@@ -1,1 +1,30 @@
+with open('list.in') as file:
+  data = [i for i in file.read().strip().split("\n")]
+
+max = 0
+max2 = 0
+max3 = 0 
+count = 0 
+
+for item in data:
+  if item == "":
+    count = 0
+  else: 
+    num = int(item)
+    count += num
+  
+  if count > max:
+    max3 = max2
+    max2 = max
+    max = count
+  elif count > max2:
+    max3 = max2
+    max2 = count
+  elif count > max3:
+    max3 = count 
+    
+
+print("Ans: ", max)
+print("Ans2: ", max+max2+max3)
+  
 
